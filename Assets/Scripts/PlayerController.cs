a53d06ec80ebb8975631e018cddc739f9e860341
bdeguzis <b.deguzis@gmail.com>
2019-02-03 14:20:07 -0500

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,11 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        GetComponent<Rigidbody>().velocity = movement;
 	}
 }
